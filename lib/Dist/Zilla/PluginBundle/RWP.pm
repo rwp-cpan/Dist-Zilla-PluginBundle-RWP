530cb73ec0352369df01b361b3fbbe8b6c0d9978
rwp0 <rwp.primary@gmail.com>
2023-02-04 22:17:15 +0400

    Rework using add_plugins and add_bundle methods

--- lib/Dist/Zilla/PluginBundle/RWP.pm
+++ lib/Dist/Zilla/PluginBundle/RWP.pm
@@ -1,33 +1,50 @@
+# ABSTRACT: Add [@RWP] plugin bundle into dist.ini
+
 use v5.37;
 
-package Dist::Zilla::PluginBundle::Example;
-
+package Dist::Zilla::PluginBundle::RWP;
 use Moose;
-
 with 'Dist::Zilla::Role::PluginBundle::Easy';
+use DDP;
 
 sub configure ( $self ) {
 
   my @plugins = qw(
     CPANFile
     AutoPrereqs
-    AutoVersion
-    PruneFiles
     NextRelease
 
     PodWeaver
     InstallGuide
-    GithubMeta
+    Git::Commit
+    Git::Tag
+  ); # Plugins added with default settings
+
+
+  $self -> add_bundle( '@Filter' => {
+    '-bundle' => '@Basic' ,
+    '-remove' => 'ConfirmRelease' ,
+  }
   );
-
-  my @bundles = qw(
-    Basic
-
-    Git
-  );
-
-  $self -> add_bundle( $_ ) for @bundles;
 
   $self -> add_plugins( @plugins );
 
+  $self -> add_plugins( AutoVersion => { major => 0 } );
+
+  $self -> add_plugins(
+    PruneFiles => {
+      filename => '_Deparsed_XSubs.pm' ,
+      filename => 'Dist::Zilla::PluginBundle::RWP.iml' ,
+    }
+  );
+
+  $self -> add_plugins( GithubMeta => { issues => 1 } );                 # External plugin
+  $self -> add_plugins( 'Git::Check' => { untracked_files => 'warn' } ); # External plugin
+
 }
+
+=pod
+
+https://metacpan.org/pod/Dist::Zilla::Role::PluginBundle::Easy
+
+https://metacpan.org/release/RJBS/Dist-Zilla-PluginBundle-RJBS-5.023/source/lib/Dist/Zilla/PluginBundle/RJBS.pm
